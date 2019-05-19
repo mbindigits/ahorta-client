7c9cc9762eea878f057e4be7c328061fc9dfc743
Alexandre Magno <alexanmtz@gmail.com>
2019-05-19 16:50:21 +0200

    Updates

---
+++ precache-manifest.e63c3702ba8b1bc349d02bc9d9ce4008.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "52bb81c94532a43de27f7317f87d9114",
+    "url": "/material-sense/static/media/shape.52bb81c9.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material-sense/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c6bd668859921708f05bf2146cf284e5",
+    "url": "/material-sense/static/media/ahorta-logo.c6bd6688.png"
+  },
+  {
+    "revision": "09f4e86c218a577ad5da",
+    "url": "/material-sense/static/js/runtime~main.09f4e86c.js"
+  },
+  {
+    "revision": "fd27b3dba07470e19b5d",
+    "url": "/material-sense/static/js/main.fd27b3db.chunk.js"
+  },
+  {
+    "revision": "448f4640f8277ae3ed0d",
+    "url": "/material-sense/static/js/1.448f4640.chunk.js"
+  },
+  {
+    "revision": "fd27b3dba07470e19b5d",
+    "url": "/material-sense/static/css/main.8b5539e3.chunk.css"
+  },
+  {
+    "revision": "0a4f1795303f7d908c5fc393104faa69",
+    "url": "/material-sense/index.html"
+  }
+];
